0a61e0fdb7b283bedd6d100300cef4b61f22525f
Antoine GIRARD <sapk@sapk.fr>
2016-12-05 16:09:31 +0100

    Write java client version

--- ClientTCP.java
+++ ClientTCP.java
@@ -1,51 +1,58 @@
+
 import java.io.*;
 import java.net.*;
 // import java.lang.*;
 
 public class ClientTCP {
-  public static void main(String[] args) throws IOException {
 
-    Socket clientSocket = null;
-    PrintWriter out = null;
-    BufferedReader in = null;
-    int port;
+    public static void main(String[] args) throws IOException {
 
-    port = Integer.parseInt(args[1]);
-    System.out.println("port : " + port);
+        Socket clientSocket = null;
+        PrintWriter out = null;
+        BufferedReader in = null;
+        int port;
 
-    try {
-      clientSocket = new  //??????????????????;
-      out = new //????????????????????????????
-      in = new //????????????????????????????
-    } catch (UnknownHostException e) {
-      System.err.println("Don't know about host: " + args[0]);
-      System.exit(1);
-    } catch (IOException e) {
-      System.err.println("Couldn't get I/O for " + "the connection to: "
-			 + args[0]);
-      System.exit(1);
+        port = Integer.parseInt(args[1]);
+        System.out.println("port : " + port);
+
+        try {
+            clientSocket = new Socket(args[0], port);
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host: " + args[0]);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for " + "the connection to: "
+                    + args[0]);
+            System.exit(1);
+        }
+
+        BufferedReader stdIn = new BufferedReader(
+                new InputStreamReader(System.in));
+        String userInput;
+        char[] buf = new char[64];
+        int r;
+
+        // Lecture de la socket
+        r = in.read(buf, 0, 64); // Attention, pas de \n envoye par le serveur
+        // donc pas de messageServeur = in.readLine();
+        System.out.print(buf);   // Affichage
+
+        while ((userInput = stdIn.readLine()) != null) {
+            // Envoi sur la socket
+            out.println(userInput);
+            out.flush();
+            // Lecture de la socket et affichage
+            // Lecture de la socket
+            r = in.read(buf, 0, 64); // Attention, pas de \n envoye par le serveur
+            // donc pas de messageServeur = in.readLine();
+            System.out.print(buf);   // Affichage
+        }
+
+        out.close();
+        in.close();
+        stdIn.close();
+        clientSocket.close();
     }
-
-    BufferedReader stdIn = new BufferedReader(
-			       new InputStreamReader(System.in));
-    String userInput;
-    char[] buf = new char[64];
-    int r;
-
-    // Lecture de la socket
-    r = in.read(buf, 0, 64); // Attention, pas de \n envoye par le serveur
-			     // donc pas de messageServeur = in.readLine();
-    System.out.print(buf);   // Affichage
-
-    while ((userInput = stdIn.readLine()) != null) {
-      // Envoi sur la socket
-      // ??????
-      // Lecture de la socket et affichage
-    }
-
-    out.close();
-    in.close();
-    stdIn.close();
-    clientSocket.close();
-  }
 }
